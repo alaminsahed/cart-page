ae0e66c4236efdc4c34b62d5b50490be9f0e9996
alaminsahed <alaminsahed97@gmail.com>
2021-10-05 22:34:13 +0600

    duplicate item bug solved

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const cardElement = document.querySelector(".card-container")
+const cardElement = document.querySelector(".card-container");
+const cartElement = document.querySelector(".cart-products");
 
 const fetchData =()=>{
     data.forEach((product)=>{
         cardElement.innerHTML += `
-        <div class="card">
+        <div class="card" onClick="addCart(${product.id})">
              <div class="card-img">
                  <img src="${product.img}" class="${product.name}">
              </div>
@@ -18,3 +19,50 @@ const fetchData =()=>{
 }
 
 fetchData();
+
+let cart=[];
+
+const addCart=(id) =>{
+   let duplicate= cart.find(item => item.id === id);
+   if(duplicate)
+   {
+       alert("product added before")
+   }
+   else{
+    const cartProduct = data.find((product)=>product.id === id);
+    //   console.log(cartProduct);
+        cart.push(cartProduct)
+      
+   }
+
+  update()
+}
+
+const update=()=>{
+    showCart();
+      
+}
+
+const showCart=()=>{
+    // console.log(cartElement.innerHTML);
+    cartElement.innerHTML = null;
+    cart.forEach((item)=>{
+        cartElement.innerHTML += `
+        <table>
+        <tr>
+          <td>
+            <div class="card cart-card">
+              <img src="${item.img}" />
+            </div>
+          </td>
+          <td>${item.name}</td>
+          <td>BDT:${item.price}</td>
+          <td><i class="fa fa-trash" aria-hidden="true"></i></td>
+        </tr>
+      </table>
+        `
+    })
+}
+
+
+
